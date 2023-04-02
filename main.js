1b0d96a2c440b410e6bb9d1a3120d47c5b253cf9
Mikko Morgan <mikko.morgan@icloud.com>
2023-04-01 23:19:47 -0400

    commit1

---
+++ main.js
@@ -0,0 +1,21 @@
+const slideshowImages = [
+    "Slideshow/image1.jpg",
+    "Slideshow/image2.jpg",
+    "Slideshow/image3.jpg",
+    "Slideshow/image4.jpg",
+    // Add more images as needed
+];
+
+let currentImageIndex = 0;
+
+function changeBackgroundImage() {
+    const slideshow = document.getElementById("slideshow");
+    slideshow.style.backgroundImage = `url(${slideshowImages[currentImageIndex]})`;
+    currentImageIndex++;
+    if (currentImageIndex >= slideshowImages.length) {
+        currentImageIndex = 0;
+    }
+}
+
+changeBackgroundImage();
+setInterval(changeBackgroundImage, 5000); // Change images every 5 seconds.
