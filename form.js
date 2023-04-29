fd566c5e31d3f0bdd9e99b029ecebbb6cbbe909f
Mikko Morgan <mikko.morgan@icloud.com>
2023-04-28 20:31:24 -0400

    commit6

--- form.js
+++ form.js
@@ -2,16 +2,15 @@ const rate = document.getElementById("rateForm");
 const contact = document.getElementById("contactForm");
 
 if (rate) {
-	rate.addEventListener('submit', () => {
-		// Display a success message
-		alert("Thank you for rating us!");
-	});
+  rate.addEventListener("submit", () => {
+    // Display a success message
+    alert("Thank you for rating us!");
+  });
 }
 
-
 if (contact) {
-	// Not called
-	contact.addEventListener("submit", () => {
-	  alert("Thank you for contacting us!");
-	});
+  // Not called
+  contact.addEventListener("submit", () => {
+    alert("Thank you for contacting us!");
+  });
 }
